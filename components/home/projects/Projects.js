a40f65d9a1d8a953a3d70b1eb080aa2474f103b2
Ahmed0702 <121123892+Ahmed0702@users.noreply.github.com>
2024-09-04 13:01:28 +0200

    Update Projects.js

--- components/home/projects/Projects.js
+++ components/home/projects/Projects.js
@@ -5,7 +5,7 @@ import styles from "./projects.module.scss";
 export const Projects = () => {
   return (
     <section className="section-wrapper" id="projects">
-      <SectionHeader title="Projects" dir="r" />
+      <SectionHeader title="Freelance projects with Upwork" dir="r" />
 
       <div className={styles.projects}>
         {projects.map((project) => {
