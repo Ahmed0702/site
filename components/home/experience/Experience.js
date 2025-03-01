e220d9acccd1e1019776602a3b3f06302e563a67
Ahmed0702 <121123892+Ahmed0702@users.noreply.github.com>
2025-03-01 23:11:38 +0100

    Update Experience.js

--- components/home/experience/Experience.js
+++ components/home/experience/Experience.js
@@ -16,7 +16,7 @@ const experience = [
   {
     title: "AStudio",
     position: "UX/UI Designer - UX Strategist",
-    time: "2020 - 2025",
+    time: "2020 - 2024",
     location: "Remote",
     description:
       "Develop design solutions based on user requirements, creating mockups, prototypes, and graphic designs in line with the brand. Collaborate with a multidisciplinary team to optimize user experience and resolve UX issues.",
