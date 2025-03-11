67b71c9bb0dadf823fc5ecea28d5466a3ede47b0
Ahmed0702 <121123892+Ahmed0702@users.noreply.github.com>
2025-03-11 22:06:49 +0100

    Update About.js

--- components/home/about/About.js
+++ components/home/about/About.js
@@ -14,17 +14,12 @@ export const About = () => {
           <Reveal>
             <p className={`${styles.aboutText} ${styles.highlightFirstLetter}`}>
               Hey! I&apos;m Ahmed, if you haven&apos;t already gathered that by
-              now. I&apos;m a web developer turned UX designer from Bologna,
-              Italy. I specialize in the backend and frontend development with such a love for the design technologies, primarily Laravel and React, but
+              now. I&apos;m a web developer and designer from Bologna,
+              Italy. I specialize in the frontend development with such a love for the design technologies, primarily Figma, InVision, Angular, Vue and React, but
               love building with whatever tools are right for the job.
             </p>
           </Reveal>
-          <Reveal>
-            <p className={styles.aboutText}>
-            My future perspective in UX design is based on low-code, where my ability to create 
-            extraordinary user experiences finds fertile ground in the ongoing digital evolution.
-            </p>
-          </Reveal>
+          
           <Reveal>
             <p className={styles.aboutText}>
               Outside of work, I love to paint, play music and football. 
@@ -34,7 +29,7 @@ export const About = () => {
           <Reveal>
             <p className={styles.aboutText}>
               I&apos;m passively looking for new positions where I can merge my
-              love for design with my love for the technology. If you think
+              love for technology and design. If you think
               you&apos;ve got an opening that I might like, let&apos;s connect
               🔗
             </p>
