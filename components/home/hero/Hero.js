b0f7574f740c0d1a89527f42ad4e534a08e982e7
Ahmed0702 <121123892+Ahmed0702@users.noreply.github.com>
2025-03-11 22:02:27 +0100

    Update Hero.js

--- components/home/hero/Hero.js
+++ components/home/hero/Hero.js
@@ -14,7 +14,7 @@ export const Hero = () => {
         </Reveal>
         <Reveal>
           <h2 className={styles.subTitle}>
-            I&apos;m a <span>Full Stack Developer and UX/UI Designer</span>
+            I&apos;m a <span>Front-End  Developer and UX/UI Designer</span>
           </h2>
         </Reveal>
         <Reveal>
